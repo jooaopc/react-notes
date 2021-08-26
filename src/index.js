04907dc77d58f5af2e0403212a5284b7c81f44c6
jooaopc <joao.ped.cs@gmail.com>
2021-08-26 15:27:40 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
+import { HashRouter, HashRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
