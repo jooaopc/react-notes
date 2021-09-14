0e52e421c5862205243ab2ff0f396150c5c26f0c
João Pedro <joao.ped.cs@gmail.com>
2021-09-14 19:39:24 -0300

    captar nota detalhada

--- src/pages/NoteDetail/index.jsx
+++ src/pages/NoteDetail/index.jsx
@@ -1,21 +1,32 @@
+import { useEffect, useState } from 'react';
 import { Card } from "react-bootstrap";
 import { useParams } from "react-router";
 
 function NoteDetail() {
     const params = useParams()
+    const [notaDetalhada, setNotaDetalhada] = useState(null) 
 
-    const notaDetalhada = JSON.parse(localStorage.getItem("notas", params.id)) || [];
-    const notaDetalhadaId = notaDetalhada.params
 
-    console.log(notaDetalhadaId)
+
+    useEffect(() => {
+      const notas = JSON.parse(localStorage.getItem("notas")) || [];
+      const nota =  notas[params.id] ?? null
+      setNotaDetalhada(nota)
+    }, [params])
 
   return (
+    <>
+      {notaDetalhada &&
     <Card className="mt-2 p-2" bg="light">
+
       <Card.Title className="justify-content-between d-flex">
         {notaDetalhada.title}
       </Card.Title>
       <Card.Body>{notaDetalhada.text}</Card.Body>
     </Card>
+      }
+    {!notaDetalhada && <p className="mt-2 p-4 card-title h4 d-flex justify-content-center" bg="light">Nota não encontrada.</p>}
+      </>
   );
 }
 
